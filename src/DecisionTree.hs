f8a144ceb6c5e6db28d857ebb251dec4a82c7296
Jonas Valfridsson <jonasvalfridsson@Jonass-iMac.local>
2018-01-23 22:31:45 +0100

    Completed a bad classifier and added some interaction

--- src/DecisionTree.hs
+++ src/DecisionTree.hs
@@ -1,21 +1,133 @@
 module DecisionTree where
 
 import Control.Arrow
+import Control.Applicative
+import Control.Monad
+import Data.List
+import Data.Function
+
+instance Monoid Double where
+  mempty = 0.0
+  mappend = (+)
+
+
+
+minIndex :: (Ord a, Monoid a, Num a) => [a] -> Int
+minIndex l = snd . foldl1 mini $ zip l [0..]
+  where
+    mini (a, i) (b, i') = case compare a b of
+                LT -> (a, i)
+                _ -> (b, i')
+
+maxMin :: (Ord a) => [a] -> (a, a)
+maxMin l = let mx = maximum l
+               mn = minimum l
+            in (mx, mn)
+
+groupOnName :: [Point] -> [[(String, [Double])]]
+groupOnName = groupBy ((==) `on` fst) . sortBy (compare `on` fst)
 
 {-Classification And Dimensionality-}
-data Group a = Group a
-  deriving (Eq, Show, Ord)
-
 type Point = (String, [Double])
 
-groupWith :: Ord b => (a -> b) -> [a] -> [[a]] 
-groupWith identifier l = groupBy ((==) `on` identifier) . sortBy (compare `on` identifier)
 
-entropy :: [(String, Double)] -> Double
-entropy separator dimension = 
-  let groups = groupWith fst dimension
-      frequency = map length groups 
-      total = length dimension
-      negativeEntropy = foldl (\l a -> a + (l / total) * logBase 2 (l / total)) 0 frequency
-   in -1 * negativeEntropy 
+entropy ::  [String] -> Double
+entropy labels = let tl = fromIntegral $ length labels
+                     ent i = (i/tl) * logBase 2 (i/tl)
+                  in negate . sum . map (ent . fromIntegral . length) . group $ sort labels 
+
+
+{-most common group-}
+mostCommonGroup :: [Point] -> [Point] 
+mostCommonGroup groups = 
+  let groups' = groupOnName groups
+      (group, _) = maximumBy (compare `on` snd) $ map (id &&& length) groups'
+    in group
+
+{- Needs to be given isolated group-}
+mostCommonAttribute :: [Point] -> Int
+mostCommonAttribute groups = minIndex $ map (attributeDelta groups) [0.. length groups]
+  where
+    attributeDelta :: [Point] -> Int -> Double
+    attributeDelta gs i = let attr = map ((!!i) . snd) gs
+                              (mx, mn) = maxMin attr
+                            in mx - mn
+    
+
+{- Partintioning Condition for Binary Tree, needs to be given the isolated group one is checking -} 
+attributeDeltaPartition :: Int -> [Point] -> (Point -> Bool)
+attributeDeltaPartition attribute groups =
+  let attrs = map ((!! attribute) . snd) groups
+      (mx, mn) = maxMin attrs
+      in \(_, attrs') -> let attr = attrs' !! attribute 
+                          in attr >= mn && attr <= mx
+
+partitionWith :: (Point -> Bool) -> [Point] -> ([Point], [Point])
+partitionWith f = foldr disperse ([], [])
+  where
+    disperse :: Point -> ([Point], [Point]) -> ([Point], [Point])
+    disperse p (a1, a2) = if f p
+                            then (p : a1, a2)
+                            else (a1, p : a2)
+
+
+
+
+data DecisionTree = Tree (Point -> DecisionTree) | Leaf String
+
+{- Most common group, does not take entropy into account, im not sure how to do that yet. -}
+binaryDistributionMCG :: [Point] -> (Point -> Bool)
+binaryDistributionMCG groups = 
+  let mcg = mostCommonGroup groups
+      mca = mostCommonAttribute mcg
+    in attributeDeltaPartition mca mcg
+
+
+classification :: [Point] -> String
+classification groups = foldr (\a b -> b ++ " or " ++ a) (head classes) (tail classes) 
+  where 
+    classes = map (fst . head) $ groupOnName groups
+
+buildTreeMCG :: [Point] -> DecisionTree
+buildTreeMCG groups = 
+    case partitionWith distribution groups of
+      ([], []) -> Leaf "Unclassifiable"
+      ([], a) -> Leaf $ classification a
+      (a, []) -> Leaf $ classification a
+      (a, b) -> Tree $ \p -> if distribution p
+                                then buildTreeMCG a
+                                else buildTreeMCG b
+  where
+    distribution = binaryDistributionMCG groups
+
+
+
+queryTree :: DecisionTree -> Point -> String
+queryTree (Leaf a) p = a
+queryTree (Tree f) p = queryTree (f p) p
+
+
+stdioQueriesNormal :: [Point] -> IO ()
+stdioQueriesNormal state = 
+  do
+    putStrLn "Building Tree.."
+    let tree = buildTreeMCG state
+
+    forever $ do
+                query <- getLine 
+                print $ queryTree tree ("", read query)
+
+
+
+stdioQueriesFile :: (FilePath -> IO [Double]) -> [Point] -> IO ()
+stdioQueriesFile mesher state =
+  do
+    putStrLn "Building Tree.."
+    let tree = buildTreeMCG state
+
+    forever $ do
+                file <- getLine
+                mesh <- mesher file
+                print $ queryTree tree ("", mesh)
+
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,96 @@
 module Main where
 
+import DecisionTree
+import BMPPROC
+import qualified System.IO.Strict as Strict
+import Control.Monad
+import System.Environment
+
+{- This is the Dependency -}
+bmpMesher :: Int -> FilePath -> IO [Double]
+bmpMesher = meshImage meanValueGrouping
+
+memoryName :: FilePath
+memoryName = "classifier.mem"
+
+type Memory = (Int, [Point])
+
+initMemory :: Point -> IO ()
+initMemory p = writeFile memoryName . show $ (length . snd $ p, [p])
+
+addClassification :: Point -> IO ()
+addClassification point = 
+  do
+    (dim, state) <- (read <$> Strict.readFile memoryName) :: IO Memory
+    if (length . snd $ point) == dim
+      then writeFile memoryName . show $ (dim, point : state)
+      else putStrLn $ "Cannot add because dimensionality does not match\nMemory: " ++ show dim ++ "\nTried to add: " ++ show (length . snd $ point)
+
+addBMP :: Int -> String -> FilePath -> IO ()
+addBMP dimensionality classification file = 
+  do
+    mesh <- bmpMesher dimensionality file
+    addClassification (classification, mesh)
+
+addByHand :: String -> [Double] -> IO ()
+addByHand name mesh = addClassification (name, mesh)
+
+
+normalQuerySession :: IO ()
+normalQuerySession =
+  do
+    (dim, mem) <- (read <$> Strict.readFile memoryName) :: IO Memory
+
+    putStrLn $ "Remember to keep dimensionality at: " ++ show dim ++ "\n"
+    stdioQueriesNormal mem
+
+
+bmpQuerySession :: IO ()
+bmpQuerySession =
+  do
+    (dim, mem) <- (read <$> Strict.readFile memoryName) :: IO Memory
+    let mesher = bmpMesher dim
+    stdioQueriesFile mesher mem
+
+normalAddSession :: IO ()
+normalAddSession = forever $
+  do
+    (classi, dims) <- (read <$> getLine) :: IO Point
+    addByHand classi dims
+
+bmpAddSession :: IO ()
+bmpAddSession = 
+  do
+    (dim, _) <- (read <$> Strict.readFile memoryName) :: IO Memory
+
+    forever $ 
+      do
+        [classi, filepath] <- words <$> getLine
+        addBMP dim classi filepath
+
+bmpInit :: IO ()  
+bmpInit = 
+  do
+    [classi, filepath, dim] <- words <$> getLine
+    mesh <- bmpMesher (read dim) filepath
+    initMemory (classi, mesh)
+
+normalInit :: IO ()
+normalInit =
+  do
+    p <- (read <$> getLine) :: IO Point
+    initMemory p
+
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = 
+  do
+    args <- getArgs
+    case args of
+      [] -> putStr "Welcome to Tree!\n\nInit Normal: -i\nAdd Normal: -a\nQuery Normal: -q\nInit Image: -im\nAdd Image: -am\nQuery Image: -qm"
+      ("-i":_) -> normalInit
+      ("-a":_) -> normalAddSession
+      ("-q":_) -> normalQuerySession
+      ("-im":_) -> bmpInit
+      ("-am":_) -> bmpAddSession
+      ("-qm":_) -> bmpQuerySession
